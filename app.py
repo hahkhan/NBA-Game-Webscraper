b3648692d9991f1df56ece7604a23a4af4ec4d7a
Haris Khan <haris.a.khan@stonybrook.edu>
2023-05-21 23:00:58 -0400

    Update to newer API and start email functionality

--- app.py
+++ app.py
@@ -1,39 +1,89 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 from flask_mail import Mail, Message
-from flask_sqlalchemy import sqlalchemy
-
+from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 import datetime
 import requests
+
 app = Flask(__name__)
-URL = "https://data.nba.net"
+app.config['MAIL_SERVER'] = "smtp.sendgrid.net"
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = "NBA Bot"
+app.config['MAIL_PASSWORD'] = os.environ.get("API_KEY")
+app.config['MAIL_USE_TLS'] = True
+app.config["MAIL_DEFAULT_SENDER"] = "nbagametracker@gmail.com"
+mail = Mail(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+SCOREBOARD_URL = "https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.String(100))
+    email = db.Column("email", db.String(120))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
 @app.route('/')
 def main_page():
     games = get_current_games()
-    return render_template("index.html", games = games)
+    return render_template("index.html", games=games)
 
 
 def get_current_games():
-    scoreboard_data = get_data()['currentScoreboard']
-    current_games = requests.get('https://data.nba.net/prod/v1/20220130/scoreboard.json').json()['games']
+    scoreboard_data = get_data()['scoreboard']
+    current_games = scoreboard_data.get("games", [])
     all_games = []
     for game in current_games:
-        game_info ={"HOME": game['hTeam'],"AWAY": game['vTeam'],"time_remaining": game['clock'],"quarter": game['period'], "ID": game['gameId']}
+        game_info = {
+            "HOME": game['homeTeam'],
+            "AWAY": game['awayTeam'],
+            "time_remaining": game['gameStatusText'].split(' ')[1],
+            "quarter": game['period'],
+            "ID": game['gameId']
+        }
         all_games.append(game_info)
     return all_games
 
 
 def get_data():
-    r = requests.get('https://data.nba.net/prod/v1/today.json')
-    return r.json()['links']
+    r = requests.get(SCOREBOARD_URL)
+    return r.json()
 
-@app.route('/email', methods = ["GET", "POST"])
+
+@app.route('/email', methods=["GET", "POST"])
 def send_emails():
-    if request.method =="POST":
-        
+    if request.method == "POST":
+        new_mail = request.form["email"]
+        if User.query.filter_by(email=new_mail).first():
+            print("User is already in the database")
+        else:
+            name = request.form["name"]
+            new_user = User(name, new_mail)
+            db.session.add(new_user)
+            db.session.commit()
+        msg = Message(
+            subject="Today's NBA Game Scores",
+            sender="nbagametracker@gmail.com",
+            recipients=[r.email for r in User.query.filter_by(email=new_mail)]
+        )
+        msg.body = render_template("index.html", games=get_current_games())
+        cur_time = datetime.datetime.now()
+        cur_time = cur_time.replace(hour=8, minute=1)
+        date_format = datetime.datetime.strptime(cur_time, "%m/%d/%Y, %H:%M:%S")
+        unix_time = datetime.datetime.timestamp(date_format)
+        msg.extra_headers = {"X-SMTPAPI": json.dumps({'send_at': unix_time})}
+        mail.send(msg)
         return "Sending Email"
     return render_template("email.html")
 
+
 if __name__ == "__main__":
-    app.run(threaded = True, debug = True)
+    with app.app_context():
+        db.create_all()
+    app.run(threaded=True, debug=True)
